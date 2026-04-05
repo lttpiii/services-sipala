643414f3683e46cc6ea98901dc3e2f63c3b27c05
Luthfi Indrawan <luthfiindrawan78@gmail.com>
2026-04-05 13:57:00 +0700

    add services borrow-approval

--- services/borrow-approval/types/approve-borrow-types.go
+++ services/borrow-approval/types/approve-borrow-types.go
@@ -1,23 +1,9 @@
 package types
 
-import "time"
-
 type (
 	ReqApproveBorrow struct {
-		BorrowID string
+		AuthUserID string
+		BorrowID   string
 	}
-	ResApproveBorrow struct {
-		ID         string         `json:"id"`
-		Status     string         `json:"status"`
-		Approvedby ApprovedbyType `json:"approved_by"`
-		ApprovedAt time.Time `json:"approved_at"`
-		BorrowDate time.Time `json:"borrow_date"`
-		DueDate time.Time `json:"due_date"`
-	}
-)
-type (
-	ApprovedbyType struct {
-		ID   string `json:"id"`
-		Name string `json:"nama"`
-	}
+	ResApproveBorrow struct{}
 )
--- services/borrow-approval/types/reject-borrow-types.go
+++ services/borrow-approval/types/reject-borrow-types.go
@@ -1,28 +1,13 @@
 package types
 
-import "time"
-
 type (
 	DTORejectBorrow struct {
 		Reason string `json:"reason"`
 	}
 	ReqRejectBorrow struct {
-		Reason string
-		BorrowID string
+		AuthUserID string
+		Reason     string
+		BorrowID   string
 	}
-	ResRejectBorrow struct {
-		ID     string `json:"id"`
-		Status string `json:"status"`
-		RejectedBy RejectedByType `json:"rejected_by"`
-		RejectedAt time.Time `json:"rejected_at"`
-		Reason string `json:"reason"`
-
-	}
-)
-
-type (
-	RejectedByType struct {
-		ID   string `json:"rejected_id"`
-		Name string `json:"name"`
-	}
+	ResRejectBorrow struct{}
 )
